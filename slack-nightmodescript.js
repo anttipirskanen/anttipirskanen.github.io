46d130192261ede0b39f7c243381951e2bf074f7
anttipirskanen <antti.a.pirskanen@gmail.com>
2020-11-27 19:35:21 +0200

    Delete slack-nightmodescript.js

--- slack-nightmodescript.js
+++
@@ -1,9 +0,0 @@
-//Append this to C:\Users\<user-name>\AppData\Local\slack\app-3.3.7\resources\app.asar.unpacked\src\static\interop.js
-document.addEventListener('DOMContentLoaded', function() {
-  $.ajax({
-    url: 'https://raw.githubusercontent.com/anttipirskanen/anttipirskanen.github.io/master/slack-custom.css',
-    success: function(css) {
-      $("<style></style>").appendTo('head').html(css);
-    }
-  });
-});
